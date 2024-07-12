6cfb27d52e7f00cfe7b7d7ba532dd805eb5a58b5
CodeSShuv <codeshuv@gmail.com>
2024-07-12 20:36:38 +0545

    gh pages setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React,{useState} from "react";
-import ReviewArea from "./Components/ReviewArea";
+import React from "react";
 import './App.css';
 
 function App() {
-  const [title,setTitle] = useState("Hotel Crystal Pashupati");
-  
   return (
     <div className="App">
       <header></header>
-      <div className="review-landing-page">
-        <h2 className = 'hotel-title'>Welcome to {title}</h2>
-        <div className = "img-container">
-         <img src="./hotelsample.jpg"/>
-         
-        </div>
-      </div>
-      <div className="review-area">
-        <ReviewArea/>
-      </div>
+      <div> </div>
     </div>
 
   );
