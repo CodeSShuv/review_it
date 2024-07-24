c1b0570be864dbb7109ea6d40fe8ef87e9792228
CodeSShuv <codeshuv@gmail.com>
2024-07-24 21:54:05 +0545

    inverted comma removed using ai

--- src/Components/ReviewArea.jsx
+++ src/Components/ReviewArea.jsx
@@ -16,11 +16,14 @@ const ReviewArea=()=>{
     
         const main = async ()=> {
          const chatCompletion = await getGroqChatCompletion();
-         // Print the completion returned by the LLM.
-       //   console.log(chatCompletion.choices[0]?.message?.content || "")
-            
-        setValue (  chatCompletion.choices[0]?.message?.content || "");
-        setDisable(false)
+        
+       
+        let filteredChat = chatCompletion.choices[0]?.message?.content || ""  ; 
+        
+        setValue (  filteredChat);
+
+        setDisable(false);
+
        
        }
        
@@ -29,7 +32,7 @@ const ReviewArea=()=>{
            messages: [
              {
                role: "user",
-               content: "write a good unique review for cafe which offers only chicken,pizza,burger,fry-rice,coffee in 3 lines without hotel name just like 13 yearold child",
+               content: "write a good unique review for cafe which offers only chicken,pizza,burger,fry-rice,coffee in 3 lines without hotel name just like 13 yearold child without double inverted comma",
              },
            ],
            model: "llama3-8b-8192",
